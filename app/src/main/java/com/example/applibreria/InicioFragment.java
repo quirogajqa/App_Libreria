e98f362b48a52d16f678d59bdf85614e175465c4
yerkoppp <ycosorio.dev@gmail.com>
2025-09-08 22:20:29 -0300

    Imágenes achicadas, incorporación de menú

---
+++ app/src/main/java/com/example/applibreria/InicioFragment.java
@@ -0,0 +1,26 @@
+package com.example.applibreria;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class InicioFragment extends Fragment {
+
+
+
+    public InicioFragment() {
+
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_inicio, container, false);
+    }
+}
